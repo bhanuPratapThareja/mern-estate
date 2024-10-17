e2f9ae5043663af95e79b8c87ad74a6ae41a5f68
Bhanu Thareja <jim.thareja@icloud.com>
2024-10-17 12:51:19 +0530

    Connected to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express()
 
-app.listen(5001, console.log('Server is running on port 5001'))
+mongoose.connect(process.env.MONGO_DB)
+    .then(() => {
+        console.log('Connected to DB Estate')
+        app.listen(5001, console.log('Server is running on port 5001'))
+    })
+    .catch((err) => console.log(err))
