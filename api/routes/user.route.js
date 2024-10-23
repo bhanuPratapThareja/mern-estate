680827a886ad136b64dcba7195da86cc442b76c4
Bhanu Thareja <jim.thareja@icloud.com>
2024-10-23 12:13:55 +0530

    get user listings route and controller

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { updateUser, deleteUser } from "../controllers/user.controller.js"
+import { updateUser, deleteUser, getUserListings } from "../controllers/user.controller.js"
 import { verifyUserToken } from '../middlewares/verifyUser.js'
 
 const router = express.Router()
 
 router.patch('/update/:id', verifyUserToken, updateUser)
 router.delete('/delete/:id', verifyUserToken, deleteUser)
+router.get('/listings/:id', verifyUserToken, getUserListings)
 
 export default router
